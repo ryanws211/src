d095644adaa485461a0de8062b0abf3fc4f6044c
Your Name <travisws@me.com>
2014-03-13 18:49:04 -0600

    travis

--- main/java/com/example/examplemod/ExampleMod.java
+++ main/java/com/example/examplemod/ExampleMod.java
@@ -9,7 +9,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 public class ExampleMod
 {
     public static final String MODID = "examplemod";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1000000.0";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
@@ -17,4 +17,6 @@ public class ExampleMod
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.func_149730_j());
     }
+    
+    //cool bob cool bob cool bob bob bob bob boj
 }
